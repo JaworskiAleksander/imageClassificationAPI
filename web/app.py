aa4a97d433819872710dfb8d245a0ed7f5c2db9a
Aleksander Jaworski <aleck156@gmail.com>
2020-12-15 11:05:11 +0100

    import bcrypt requests subprocess and json packages in app.py

--- web/app.py
+++ web/app.py
@@ -1,3 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resources
 from pymongo import MongoClient
+import bcrypt
+# necessary to get the images from URL's provided by users
+import requests
+import subprocess
+import json
