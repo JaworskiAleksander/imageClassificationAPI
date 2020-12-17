094c12264edeabb9173258861012d4be0d5082f9
Aleksander Jaworski <aleck156@gmail.com>
2020-12-17 10:55:03 +0100

    code formatting

--- web/app.py
+++ web/app.py
@@ -169,7 +169,7 @@ class Refill(Resources):
             },
             {
                 '$inc': {
-                    'Tokens': amount
+                    'Tokens':   amount
                 }
             }
         )
