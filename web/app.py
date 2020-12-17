5d1c94c19e97c37b7aace5045b39cfd70ec9be1f
Aleksander Jaworski <aleck156@gmail.com>
2020-12-17 11:00:19 +0100

    run app when __name__ is __main__

--- web/app.py
+++ web/app.py
@@ -181,3 +181,6 @@ class Refill(Resources):
 api.add_resource(Register, '/register')
 api.add_resource(Classify, '/classify')
 api.add_resource(Refill, '/refill')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
